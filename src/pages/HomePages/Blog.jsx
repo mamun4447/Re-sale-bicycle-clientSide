9cfb1d711d7fb8bc19a3610f3ec0b1e61b9658e2
mamun4447 <abdullah15-12557@diu.edu.bd>
2022-11-28 21:09:17 +0600

    blogs

--- src/pages/HomePages/Blog.jsx
+++ src/pages/HomePages/Blog.jsx
@@ -36,11 +36,12 @@ const Blog = () => {
                 </h1>
 
                 <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  SQL is the programming language used to interface with
-                  relational databases. (Relational databases model data as
-                  records in rows and tables with logical links between them).
-                  NoSQL is a class of DBMs that are non-relational and generally
-                  do not use SQL.
+                  There are four main types of state you need to properly manage
+                  in your React apps: <br />
+                  1. Local state <br />
+                  2. Global state <br />
+                  3. Server state <br />
+                  4. URL state
                 </p>
               </div>
             </div>
@@ -69,11 +70,12 @@ const Blog = () => {
                 </h1>
 
                 <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  What is JWT (JSON Web Token)? JSON Web Token (JWT) is an open
-                  standard (RFC 7519) for securely transmitting information
-                  between parties as JSON object. It is compact, readable and
-                  digitally signed using a private key/ or a public key pair by
-                  the Identity Provider(IdP).
+                  The Prototypal Inheritance is a feature in javascript used to
+                  add methods and properties in objects. It is a method by which
+                  an object can inherit the properties and methods of another
+                  object. Traditionally, in order to get and set the
+                  [[Prototype]] of an object, we use Object. getPrototypeOf and
+                  Object.
                 </p>
               </div>
             </div>
@@ -102,10 +104,11 @@ const Blog = () => {
                 </h1>
 
                 <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  JavaScript is a simple programming language that can be used
-                  with any browser that has the JavaScript Engine installed.
-                  Node. js, on the other hand, is an interpreter or execution
-                  environment for the JavaScript programming language.
+                  The main objective of unit testing is to isolate written code
+                  to test and determine if it works as intended. Unit testing is
+                  an important step in the development process, because if done
+                  correctly, it can help detect early flaws in code which may be
+                  more difficult to find in later testing stages.
                 </p>
               </div>
             </div>
@@ -134,11 +137,24 @@ const Blog = () => {
                 </h1>
 
                 <p className="mt-2 text-sm text-gray-500 dark:text-gray-300">
-                  How NodeJS handle multiple client requests? NodeJS receives
-                  multiple client requests and places them into EventQueue.
-                  NodeJS is built with the concept of event-driven architecture.
-                  NodeJS has its own EventLoop which is an infinite loop that
-                  receives requests and processes them.
+                  <span className="font-bold">React: </span>
+                  React doesn’t enforce a specific project structure, and as you
+                  can see from the official “Hello World” example below, you can
+                  start using React with just a few lines of code. <br />
+                  <br />
+                  <span className="font-bold">Vue: </span>
+                  The Vue.js core library focuses on the View layer only. It’s
+                  called a progressive framework because you can extend its
+                  functionality with official and third-party packages, such as
+                  Vue Router or Vuex, to turn it into an actual framework.{" "}
+                  <br /> <br />
+                  <span className="font-bold">Angular: </span>
+                  In this article, I’m discussing Angular 2, and not the first
+                  version of the framework which is now known as AngularJS.
+                  AngularJS, the original framework, is an MVC
+                  (Model-View-Controller)) framework. But in Angular 2, there’s
+                  no strict association with MV*-patterns as it is also
+                  component-based.
                 </p>
               </div>
             </div>
