aae0e219736cc47fa351e90d757736cc2ad54378
mamun4447 <abdullah15-12557@diu.edu.bd>
2022-11-28 13:33:27 +0600

    Add product

--- src/pages/Dashboard/MyProducts.jsx
+++ src/pages/Dashboard/MyProducts.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { AuthContext } from "../../Context/AuthProvider";
 
 const MyProducts = () => {
+  const { user } = useContext(AuthContext);
+
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/products/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, [user?.email]);
   return <div></div>;
 };
 
